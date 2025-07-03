dc4d2054bbba44cbf06f6028695e84f003d63da4
MewKay <miorakanto25z@gmail.com>
2025-07-04 00:29:29 +0300

    Display post list inside Blog list component

--- src/app/blog-list/blog-list.jsx
+++ src/app/blog-list/blog-list.jsx
@@ -1,3 +1,26 @@
-const BlogList = () => <>List of blogs</>;
+import { useEffect, useState } from "react";
+import postService from "@/services/post.service";
+
+const BlogList = () => {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    postService.getAll().then((postList) => setPosts(postList));
+  }, []);
+
+  return (
+    <ul>
+      {posts.map((post) => (
+        <li key={post.id}>
+          <h3>{post.title}</h3>
+          <p>
+            by @{post.author.username} at {post.created_at}
+          </p>
+          <p>{post.text}</p>
+        </li>
+      ))}
+    </ul>
+  );
+};
 
 export default BlogList;
--- src/app/blog-list/blog-list.test.jsx
+++ src/app/blog-list/blog-list.test.jsx
@@ -1,11 +1,47 @@
-import { describe, expect, it } from "vitest";
-import { render } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
 import BlogList from "./blog-list";
 
-describe("Blog list component", () => {
-  it("renders correclty", () => {
-    const { container } = render(<BlogList />);
+const mockPostList = [
+  {
+    id: 1,
+    title: "This is my first post",
+    text: "This is the body of my post",
+    created_at: new Date(2025, 4, 16).toISOString(),
+    author: {
+      username: "Some_random_person",
+    },
+  },
+  {
+    id: 2,
+    title: "Art of testing",
+    text: "Testing is mandatory, nuff said",
+    created_at: new Date(2023, 8, 21).toISOString(),
+    author: {
+      username: "thu_sun",
+    },
+  },
+];
 
-    expect(container).toMatchSnapshot();
+vi.mock("@/services/post.service.js", () => ({
+  default: {
+    getAll: vi.fn(() => Promise.resolve(mockPostList)),
+  },
+}));
+
+describe("Blog list component", () => {
+  it("fetched posts list", async () => {
+    render(<BlogList />);
+    const firstPostTitle = new RegExp(mockPostList[0].title, "i");
+    const secondPostTitle = new RegExp(mockPostList[1].title, "i");
+
+    const postList = await screen.findAllByRole("listitem");
+    const firstPost = await screen.findByText(firstPostTitle);
+    const secondPost = await screen.findByText(secondPostTitle);
+
+    screen.debug();
+    expect(postList).toHaveLength(mockPostList.length);
+    expect(firstPost).toBeInTheDocument();
+    expect(secondPost).toBeInTheDocument();
   });
 });
