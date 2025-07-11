72cbe8da4c7eb3d310dae27a9ffd2c8747a998c4
MewKay <miorakanto25z@gmail.com>
2025-07-11 19:22:19 +0300

    Display error message on blog post page on invalid url

--- src/app/blog-post/blog-post.jsx
+++ src/app/blog-post/blog-post.jsx
@@ -1,4 +1,4 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Post from "@/features/post/post";
 import sqids from "@/lib/sqids";
 
@@ -6,6 +6,17 @@ const BlogPost = () => {
   const { encodedId } = useParams();
 
   const postId = sqids.decode(encodedId);
+
+  if (!postId) {
+    return (
+      <main>
+        <p>
+          Sorry, this post could not be found.
+          <Link to={"/"}>Go back to the home page.</Link>
+        </p>
+      </main>
+    );
+  }
 
   return (
     <main>
---
+++ src/app/blog-post/blog-post.test.jsx
@@ -0,0 +1,31 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import sqids from "@/lib/sqids";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import routes from "../routes";
+
+vi.mock("../home/home.jsx", () => ({
+  default: () => <p>This is home</p>,
+}));
+
+describe("Blog Post page", () => {
+  it("display link to home if post is not found", async () => {
+    sqids.decode = vi.fn().mockReturnValueOnce(null);
+    const user = userEvent.setup();
+
+    const router = createMemoryRouter(routes, {
+      initialEntries: ["/posts/:id/:slug"],
+    });
+
+    render(<RouterProvider router={router} />);
+
+    const homeLink = screen.getByRole("link", { name: /home/i });
+    expect(homeLink).toBeInTheDocument();
+
+    await user.click(homeLink);
+    const homeText = await screen.findByText("This is home");
+
+    expect(homeText).toBeInTheDocument();
+  });
+});
