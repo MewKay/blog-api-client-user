19958f9fd689efbe053b91b41765375933b2efa1
MewKay <miorakanto25z@gmail.com>
2025-07-16 14:55:49 +0300

    Create log in form

--- src/app/login/login.jsx
+++ src/app/login/login.jsx
@@ -1,34 +1,10 @@
-import { useState } from "react";
-import { Form } from "react-router-dom";
+import LoginForm from "@/features/login-form/login-form";
 
 const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
   return (
-    <Form method="post">
-      <label>
-        Username
-        <input
-          type="text"
-          name="username"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </label>
-
-      <label>
-        Password
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </label>
-
-      <button>Log in</button>
-    </Form>
+    <main>
+      <LoginForm />
+    </main>
   );
 };
 
---
+++ src/features/login-form/login-form.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+import { Form } from "react-router-dom";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <Form method="post">
+      <label>
+        Username
+        <input
+          type="text"
+          name="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        />
+      </label>
+
+      <label>
+        Password
+        <input
+          type="password"
+          name="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        />
+      </label>
+
+      <button>Log in</button>
+    </Form>
+  );
+};
+
+export default LoginForm;
