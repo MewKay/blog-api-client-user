bede9e21f48ea5b5353c23bd6e8af1d7a942389b
MewKay <miorakanto25z@gmail.com>
2025-07-21 12:35:09 +0300

    Add test to home page

---
+++ src/app/pages/home/home.test.jsx
@@ -0,0 +1,52 @@
+import { describe, it, expect, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
+import {
+  createMemoryRouter,
+  RouterProvider,
+  useLoaderData,
+} from "react-router-dom";
+import routes from "@/app/routes/routes";
+import mockPosts from "@/testing/mocks/posts";
+import postService from "@/services/post.service";
+import userEvent from "@testing-library/user-event";
+
+vi.mock("@/app/layout/header/header.jsx", () => ({
+  default: () => <>This is header</>,
+}));
+
+vi.mock("../blog-post/blog-post.jsx", () => ({
+  default: function MockBlogPost() {
+    const { post } = useLoaderData();
+    return (
+      <>
+        <p>This is blog post of {post.author.username}</p>
+      </>
+    );
+  },
+}));
+
+describe("Home page", () => {
+  it("should route to the correct blog post on post item click", async () => {
+    const mockPost = mockPosts[0];
+
+    postService.getAll = vi.fn().mockResolvedValueOnce(mockPosts);
+    postService.getById = vi.fn().mockResolvedValueOnce(mockPost);
+
+    const user = userEvent.setup();
+    const router = createMemoryRouter(routes, { initialEntries: ["/"] });
+    render(<RouterProvider router={router} />);
+
+    const mockPostLink = await screen.findByRole("link", {
+      name: new RegExp(mockPost.title),
+    });
+
+    await user.click(mockPostLink);
+    const mockBlogPostText = await screen.findByText(
+      `This is blog post of ${mockPost.author.username}`,
+    );
+
+    screen.debug();
+
+    expect(mockBlogPostText).toBeInTheDocument();
+  });
+});
