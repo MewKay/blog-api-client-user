ae6eeaebb3db0a9542b98963953b895dff61d72d
MewKay <miorakanto25z@gmail.com>
2025-07-24 07:21:07 +0300

    Update app.test.jsx
    Created App render helper to use the MemoryRouter to re-initialize the
    app state on eacch test case.
    Added sign up routing test case.

--- src/app/app.test.jsx
+++ src/app/app.test.jsx
@@ -1,28 +1,41 @@
 import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
-import App from "./app";
+import { AuthProvider } from "@/context/auth.context";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import routes from "./routes/routes";
 
-vi.mock("./pages/home/home.jsx", () => ({
-  default: () => <>This is home page</>,
-}));
+vi.mock("./pages/home/home.jsx");
 
 vi.mock("./pages/login/login.jsx", () => ({
   default: () => <>This is log in page</>,
 }));
 
+vi.mock("./pages/signup/signup.jsx", () => ({
+  default: () => <>This is sign up page</>,
+}));
+
+const renderApp = () => {
+  const router = createMemoryRouter(routes, { initialEntries: ["/"] });
+  render(
+    <AuthProvider>
+      <RouterProvider router={router} />
+    </AuthProvider>,
+  );
+};
+
 describe("App component", () => {
   it("should render home page by default", () => {
-    render(<App />);
+    renderApp();
 
-    const homeText = screen.getByText(/This is home page/);
+    const homeText = screen.getByText(/This is home/);
 
     expect(homeText).toBeInTheDocument();
   });
 
-  it("should render log in page on link click", async () => {
+  it("should render log in page on header link click", async () => {
     const user = userEvent.setup();
-    render(<App />);
+    renderApp();
 
     const loginLink = screen.getByRole("link", { name: /log in/i });
 
@@ -33,5 +46,20 @@ describe("App component", () => {
 
     expect(homeText).not.toBeInTheDocument();
     expect(loginText).toBeInTheDocument();
+  });
+
+  it("should render sign up page on header link click", async () => {
+    const user = userEvent.setup();
+    renderApp();
+
+    const signUpLink = screen.getByRole("link", { name: /sign up/i });
+
+    await user.click(signUpLink);
+
+    const homeText = screen.queryByText(/This is home page/);
+    const signUpText = await screen.findByText(/This is sign up page/);
+
+    expect(homeText).not.toBeInTheDocument();
+    expect(signUpText).toBeInTheDocument();
   });
 });
