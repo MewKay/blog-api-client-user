1bb9eb4732cc703510db7348d9b7d5511c873ea5
MewKay <miorakanto25z@gmail.com>
2025-07-25 01:22:07 +0300

    Add createOne to comment service

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -2,6 +2,8 @@ import api from "./api-client";
 
 const commentService = {
   getAllByPostId: (postId) => api.get(`/posts/${postId}/comments`),
+  createOne: ({ postId, text }, token) =>
+    api.post(`/posts/${postId}/comments`, { text }, token),
 };
 
 export default commentService;
