1c08f0f6d93557a09455f822c5c01a2fab181609
MewKay <miorakanto25z@gmail.com>
2025-07-25 02:03:35 +0300

    Update new comment input component
    Added logic to form submit.

--- src/features/comment-write/new-comment-input/new-comment-input.jsx
+++ src/features/comment-write/new-comment-input/new-comment-input.jsx
@@ -1,7 +1,25 @@
+import { useParams } from "react-router-dom";
+import authService from "@/services/auth.service";
+import commentService from "@/services/comment.service";
+import sqids from "@/lib/sqids";
+
 const NewCommentInput = () => {
+  const { encodedId } = useParams();
+
+  const handleCommentSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    const postId = sqids.decode(encodedId);
+    const text = formData.get("text");
+    const token = authService.getToken();
+
+    await commentService.createOne({ postId, text }, token);
+  };
+
   return (
-    <form>
-      <textarea placeholder="Write a new comment..."></textarea>
+    <form onSubmit={handleCommentSubmit}>
+      <textarea name="text" placeholder="Write a new comment..."></textarea>
       <button>Send</button>
     </form>
   );
--- src/features/comment-write/new-comment-input/new-comment-input.test.jsx
+++ src/features/comment-write/new-comment-input/new-comment-input.test.jsx
@@ -1,11 +1,49 @@
-import { describe, expect, it } from "vitest";
-import { render } from "@testing-library/react";
+import { describe, expect, it, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
 import NewCommentInput from "./new-comment-input";
+import userEvent from "@testing-library/user-event";
+import sqids from "@/lib/sqids";
+import commentService from "@/services/comment.service";
+import authService from "@/services/auth.service";
+
+const mockPostId = 5;
+const mockToken = "LetMeIn";
+const userComment = "Great post! Post more!";
 
 describe("NewCommentInput component", () => {
   it("should render correctly", () => {
     const { container } = render(<NewCommentInput />);
 
     expect(container).toMatchSnapshot();
+  });
+
+  it("should be able to type on textbox", async () => {
+    const user = userEvent.setup();
+    render(<NewCommentInput />);
+
+    const commentTextBox = screen.getByPlaceholderText(/new comment/i);
+    await user.type(commentTextBox, userComment);
+
+    expect(commentTextBox).toHaveValue(userComment);
+  });
+
+  it("should send the correct postId and text with an auth token", async () => {
+    sqids.decode = vi.fn().mockReturnValueOnce(mockPostId);
+    authService.getToken = vi.fn().mockReturnValueOnce(mockToken);
+    commentService.createOne = vi.fn();
+
+    const user = userEvent.setup();
+    render(<NewCommentInput />);
+
+    const commentTextBox = screen.getByPlaceholderText(/new comment/i);
+    const submitButton = screen.getByRole("button", { name: /send/i });
+
+    await user.type(commentTextBox, userComment);
+    await user.click(submitButton);
+
+    expect(commentService.createOne).toHaveBeenCalledWith(
+      { postId: mockPostId, text: userComment },
+      mockToken,
+    );
   });
 });
