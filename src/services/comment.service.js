1534b7acdb506ba43424e8d6175ce9227f59bc3c
MewKay <miorakanto25z@gmail.com>
2025-07-31 23:30:09 +0300

    Add updateOne to comment service

--- src/services/comment.service.js
+++ src/services/comment.service.js
@@ -4,6 +4,8 @@ const commentService = {
   getAllByPostId: (postId) => api.get(`/posts/${postId}/comments`),
   createOne: ({ postId, text }, token) =>
     api.post(`/posts/${postId}/comments`, { text }, token),
+  updateOne: ({ postId, commentId, text }, token) =>
+    api.put(`/posts/${postId}/comments/${commentId}`, { text }, token),
 };
 
 export default commentService;
