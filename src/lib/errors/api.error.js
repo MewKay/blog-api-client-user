0d9b87586e32f50487685a910b6d424f32ec20a5
MewKay <miorakanto25z@gmail.com>
2025-08-07 15:42:30 +0300

    Create API error

---
+++ src/lib/errors/api.error.js
@@ -0,0 +1,10 @@
+class APIError extends Error {
+  constructor(message, status, response) {
+    super(message);
+    this.name = "APIError";
+    this.status = status;
+    this.response = response;
+  }
+}
+
+export default APIError;
