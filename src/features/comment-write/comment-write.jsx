d46e39777c028848d9a6190f4525b5622f0b90f4
MewKay <miorakanto25z@gmail.com>
2025-10-09 09:33:24 +0300

    Style CommentWrite component
    Add border separators to global styles.

--- src/features/comment-write/comment-write.jsx
+++ src/features/comment-write/comment-write.jsx
@@ -3,13 +3,14 @@ import AuthPrompt from "./auth-prompt/auth-prompt";
 import NewCommentInput from "./new-comment-input/new-comment-input";
 import useBlogComments from "@/hooks/useBlogComments";
 import EditCommentInput from "./edit-comment-input/edit-comment-input";
+import styles from "./comment-write.module.css";
 
 const CommentWrite = () => {
   const { isAuthenticated } = useAuth();
   const { commentToEdit } = useBlogComments();
 
   return (
-    <div>
+    <div className={styles.container}>
       {isAuthenticated ? (
         !commentToEdit ? (
           <NewCommentInput />
